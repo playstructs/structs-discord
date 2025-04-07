aeadfaff51bf9dc11fc1b9119cec494594a0f5c9
abstrct <abstrct@slow.ninja>
2025-04-07 13:57:52 -0400

    Fixing cmd directory issue

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Require the necessary discord.js classes
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
 
 // Discord Token
@@ -17,17 +15,8 @@ const client = new Client({
     ]
 });
 
-client.commands = new Collection();
-
-// Load commands
-const commandsPath = path.join(__dirname, 'src/commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    client.commands.set(command.data.name, command);
-}
+// Load commands from src/commands/index.js
+client.commands = require('./src/commands');
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
